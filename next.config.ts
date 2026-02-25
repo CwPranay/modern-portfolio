9c6e8dec031e04e38b6f249659e83a48f8f1d476
Pranay Gurav <guravpranay81@gmail.com>
2026-02-25 20:41:02 +0530

    fixes in next config

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output:'export',
+  images:{
+    unoptimized:true
+  },
   reactStrictMode: true,
   devIndicators:false,
+  basePath: '/modern-portfolio',
+  assetPrefix: '/modern-portfolio/',
+  trailingSlash: true,
 };
 
 export default nextConfig;
